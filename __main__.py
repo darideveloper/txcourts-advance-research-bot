1d289293da118c98cb491df363669952e28373f6
daridev <darideveloper@gmail.com>
2024-09-18 12:19:08 -0600

    initial repo

---
+++ __main__.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+from libs.web_scraping import WebScraping
+
+load_dotenv()
+VAR = os.getenv("VAR")
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
