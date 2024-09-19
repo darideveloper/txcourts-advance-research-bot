ba9e1767144e317aa150433a06b38fb8f4c294c4
daridev <darideveloper@gmail.com>
2024-09-19 10:56:09 -0600

    feat (data_manager): get_input_data

--- __main__.py
+++ __main__.py
@@ -1,14 +1,30 @@
 import os
 from dotenv import load_dotenv
-from scraper import Scraper
 
+from scraper import Scraper
+from data_manager import DataManager
+
+# Env variables
 load_dotenv()
-VAR = os.getenv("VAR")
+GOOGLE_SHEET_LINK = os.getenv("GOOGLE_SHEET_LINK")
+SHEET_INPUT = os.getenv("SHEET_INPUT")
+SHEET_OUTPUT = os.getenv("SHEET_OUTPUT")
+
+# Paths
+current_path = os.path.dirname(os.path.abspath(__file__))
+creds_path = os.path.join(current_path, "credentials.json")
 
 
 def main():
-    scraper = Scraper()
-    scraper.get_case_data("017-355108-24", "7/31/2024")
+    # Main workflow
+    
+    # scraper = Scraper()
+    # scraper.get_case_data("017-355108-24", "7/31/2024")
+    
+    data_manager = DataManager(GOOGLE_SHEET_LINK, creds_path,
+                               SHEET_INPUT, SHEET_OUTPUT)
+    input_data = data_manager.get_input_data()
+    print()
     
 
 if __name__ == "__main__":
---
+++ data_manager.py
@@ -0,0 +1,60 @@
+import os
+from libs.google_sheets import SheetsManager
+
+
+class DataManager(SheetsManager):
+    
+    def __init__(self, google_sheet_link: str, creds_path: os.PathLike,
+                 sheet_input: str, sheet_output: str):
+        """ Class to manage data from google sheet
+
+        Args:
+            google_sheet_link (str): editable google sheet link
+            creds_path (os.PathLike): path to google json credentials file
+            sheet_input (str): name of the input sheet
+            sheet_output (str): name of the output sheet
+        """
+        
+        # Save sheets and columns
+        self.sheet_input = sheet_input
+        self.sheet_output = sheet_output
+        
+        super().__init__(google_sheet_link, creds_path)
+        
+    def get_input_data(self) -> list[dict[str, str]]:
+        """ Get cases data from input sheet
+
+        Returns:
+            list[dict[str, str]]: list of cases data
+        """
+        
+        print("Getting input data...")
+    
+        # Activate input sheet
+        self.set_sheet(self.sheet_input)
+        
+        # Get all values from the sheet
+        data = self.get_data()
+        data_ready = list(filter(lambda row: row["Status"] == "ready", data))
+        
+        # Filter only ready registers and required columns
+        return data_ready
+    
+    def write_output_row(self, row_data: list[str]):
+        """ Write case row in output sheet
+
+        Args:
+            row_data (list[str]): row data
+        """
+        pass
+    
+    def update_input_status(self, row: int, status: str):
+        """ Update status of case input row
+
+        Args:
+            row (int): row number
+            status (str): status
+        """
+        pass
+
+    
